1761ce37421f61a79a7a7b5cd8df503509c161c7
Archana Bansal <archanamittal0388@gmail.com>
2018-03-19 20:47:07 +0530

    Feat: Handled nested expression

--- lisp.js
+++ lisp.js
@@ -29,39 +29,50 @@ const operators = {
 }
 
 function expressionParser (input) {
-  if (input[0] === '(') {
-    input = input.slice(1)
-    let arr = []
-    input = trimSpaces(input)
-    // saving the operator in currentOpertor variable
-    let currentOperator = operators[input[0]]
-    if (currentOperator) {
-      input = input.slice(1)
-      input = trimSpaces(input)
-      // extracting numbers and pushing it to arr
-      while (input[0] !== ')') {
-        let numOutPut = numberParser(input)
-        arr.push(numOutPut[0])
-        input = input.slice(1)
-        input = trimSpaces(input)
-      }
-      if (input[0] === ')') {
-        input = input.slice(1)
-      }
-      return [currentOperator(arr), input]
-    }
+  if (input[0] !== '(') {
+    return null
   }
-  return null
+
+  input = input.slice(1)
+  let arr = []
+  input = trimSpaces(input)
+  let currentOperator = operators[input[0]]
+  if (!currentOperator) {
+    return null
+  }
+
+  input = input.slice(1)
+  input = trimSpaces(input)
+  while (input[0] !== ')') {
+    input = trimSpaces(input)
+    /*
+     * Recursively call when it encounters '('
+     */
+    if (input[0] === '(') {
+      const result = expressionParser(input)
+      arr.push(result[0])
+      input = result[1]
+      continue
+    }
+    let numOutPut = numberParser(input)
+    arr.push(numOutPut[0])
+    input = numOutPut[1]
+    input = trimSpaces(input)
+  }
+
+  if (input[0] === ')') {
+    input = input.slice(1)
+  }
+  return [currentOperator(arr), input]
 }
 
 // console.log(expressionParser('(+ 1 1)'))
-// console.log(expressionParser('(+ 1 2 3)'))
-// console.log(expressionParser('(- 1 1)'))
-// console.log(expressionParser('(* 2 2 2)'))
-// console.log(expressionParser('(/ 8 4)'))
 // console.log(expressionParser('(+ 8 1 0 9 0)'))
 // console.log(expressionParser('(+ 2 3 5)'))
 // console.log(expressionParser('(- 4 3 1)'))
 // console.log(expressionParser('(* 2 3 2)'))
 // console.log(expressionParser('(/ 4 2 2)'))
 // console.log(expressionParser('(1 2)'))
+// console.log(expressionParser('(+ (+ 1 2) (+ 3 4) 87)'))
+// console.log(expressionParser('(- (+ 1 2 9 9) (* 3 4) 87)'))
+// console.log(expressionParser('(+ (* 6 9) (/ 9 4) 9)'))
